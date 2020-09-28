a1022d3d3b21ae44dbb29fbf066cc92330c847ce
Brian O'Meara <omeara.brian@gmail.com>
2020-09-28 15:14:15 -0400

    did not commit the right changes

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -1,9 +1,9 @@
 test_that("Reading names from the web works", {
-  expect_true(any(grepl("Moran", extract_names(con="http://www.nasonline.org/member-directory/living-member-list.html"))))
+  expect_true(any(grepl("Moran", extract_people(con="http://www.nasonline.org/member-directory/living-member-list.html"))))
 })
 
 test_that("Reading names from text works", {
-  expect_true(any(grepl("Moran", extract_names(text="A key person working on symbiosis is Nancy Moran."))))
+  expect_true(any(grepl("Moran", extract_people(text="A key person working on symbiosis is Nancy Moran."))))
 })
 
 test_that("Pulling data from ASMD works", {
@@ -14,7 +14,7 @@ test_that("Pulling data from ASMD works", {
 
 test_that("True matches work", {
 	asmd <- get_misconduct(agree=TRUE)
-	nasem <- extract_names(con="http://www.nasonline.org/member-directory/living-member-list.html")
+	nasem <- extract_people(con="http://www.nasonline.org/member-directory/living-member-list.html")
 	apparent_matches <- match_misconduct(nasem, asmd)
 	expect_true(ncol(asmd)+1 == ncol(apparent_matches))
 	expect_true(any(grepl("Marcy", apparent_matches$Pool)))
